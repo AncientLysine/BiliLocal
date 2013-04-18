ea3d7e2708c15e1b248d90786da5c76d4ee086da
HuangKangJing <huangkangjing@gmail.com>
2013-04-18 09:54:19 +0800

    Updated new UI translations.

--- trans/zh_CN.ts
+++ trans/zh_CN.ts
@@ -4,55 +4,60 @@
 <context>
     <name>Menu</name>
     <message>
-        <location filename="Menu.cpp" line="30"/>
-        <location filename="Menu.cpp" line="111"/>
+        <location filename="../src/Menu.cpp" line="61"/>
+        <location filename="../src/Menu.cpp" line="141"/>
         <source>Open</source>
         <translation>打开</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="31"/>
-        <location filename="Menu.cpp" line="117"/>
+        <location filename="../src/Menu.cpp" line="62"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <location filename="../src/Menu.cpp" line="63"/>
+        <location filename="../src/Menu.cpp" line="147"/>
         <source>Load</source>
         <translation>加载</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="34"/>
-        <location filename="Menu.cpp" line="46"/>
+        <location filename="../src/Menu.cpp" line="66"/>
+        <location filename="../src/Menu.cpp" line="75"/>
         <source>Open File</source>
         <translation>打开文件</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="46"/>
+        <location filename="../src/Menu.cpp" line="75"/>
         <source>XML files (*.xml)</source>
         <translation>XML文件 (*.xml)</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="59"/>
+        <location filename="../src/Menu.cpp" line="87"/>
         <source>Danmaku Alpha</source>
         <translation>透明度</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="68"/>
+        <location filename="../src/Menu.cpp" line="96"/>
         <source>Danmaku Delay</source>
         <translation>弹幕延迟</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="81"/>
+        <location filename="../src/Menu.cpp" line="109"/>
         <source>Danmaku Power</source>
         <translation>弹幕增压</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="104"/>
+        <location filename="../src/Menu.cpp" line="134"/>
         <source>Local XML File</source>
         <translation>本地弹幕</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="123"/>
+        <location filename="../src/Menu.cpp" line="153"/>
         <source>Protect Sub</source>
         <translation>保护字幕</translation>
     </message>
     <message>
-        <location filename="Menu.cpp" line="135"/>
+        <location filename="../src/Menu.cpp" line="165"/>
         <source>Font</source>
         <translation>弹幕字体</translation>
     </message>
@@ -60,12 +65,12 @@
 <context>
     <name>State</name>
     <message>
-        <location filename="State.cpp" line="21"/>
+        <location filename="../src/State.cpp" line="47"/>
         <source>Time</source>
         <translation>时间</translation>
     </message>
     <message>
-        <location filename="State.cpp" line="22"/>
+        <location filename="../src/State.cpp" line="48"/>
         <source>Volume</source>
         <translation>音量</translation>
     </message>
